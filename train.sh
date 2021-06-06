7d0fc37996b2069a8b378671f1d5ca70b24e88cb
Volkan Aydıngül <volkanaydingul@gmail.com>
2021-06-06 17:28:52 +0300

    CUDA error fix wip

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 name=cylinder_asym_networks_sparse_conv_lstm
 gpuid=0
 
-CUDA_VISIBLE_DEVICES=${gpuid}  python3 train_cylinder_asym_cuda.py \
+CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=${gpuid}  python3 train_cylinder_asym_cuda.py \
 2>&1 | tee logs_dir/${name}_logs_tee.txt
